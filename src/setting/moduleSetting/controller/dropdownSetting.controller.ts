9ebcc20df63ed2d7dd215924aef9e2550790c723
balan081194 <balancivil9133@gmail.com>
2024-06-07 17:15:56 +0530

    ini

--- src/setting/moduleSetting/controller/dropdownSetting.controller.ts
+++ src/setting/moduleSetting/controller/dropdownSetting.controller.ts
@@ -79,100 +79,132 @@ export let createCustomLabel = async (req, res, next) => {
 // }
 
 
+// export const updateDropDownList = async (req, res) => {
+//     const errors = validationResult(req)
+//     if (errors.isEmpty()) {
+//     try {
+//         const DropdownListDetails: DropDownListDocument = req.body;
+//         let staffData = await DropDownList.findByIdAndUpdate({ _id: req.query._id }, {
+//             $set: {
+//                 courseType: DropdownListDetails.courseType,
+//                 popularCategories: DropdownListDetails.popularCategories,
+//                 country: DropdownListDetails.country,
+//                 offerTAT: DropdownListDetails.offerTAT,
+//                 institutionType: DropdownListDetails.institutionType,
+//                 paymentMethod: DropdownListDetails.paymentMethod,
+//                 tax: DropdownListDetails.tax,
+//                 commissionPaidOn: DropdownListDetails.commissionPaidOn,
+//                 typeOfClient: DropdownListDetails.typeOfClient,
 
-export let updateDropDownList = async (req, res, next) => {
-    const errors = validationResult(req);
-    if (errors.isEmpty()) {
+//                 modifiedOn: DropdownListDetails.modifiedOn,
+//                 modifiedBy: DropdownListDetails.modifiedBy,
+//             }
+//         })
+
+//             response(req, res, activity, 'Level-2', 'Update-DropdownList', true, 200, staffData, clientError.success.updateSuccess);
+//         } catch (err: any) {
+//             response(req, res, activity, 'Level-3', 'Update-DropdownList', false, 500, {}, errorMessage.internalServer, err.message);
+//         }
+//     }
+//     else {
+//         response(req, res, activity, 'Level-3', 'Update-DropdownList', false, 422, {}, errorMessage.fieldValidation, JSON.stringify(errors.mapped()));
+//     }
+// }
+
+
+    export let updateDropDownList = async (req, res, next) => {
+        const errors = validationResult(req);
+        if (errors.isEmpty()) {
+            try {
+                const DropdownListDetails: DropDownListDocument = req.body;
+                const dropdown = await DropDownList.findOne({ $and: [{ _id: { $ne: DropdownListDetails._id }}] });
+
+                if (!dropdown ) {
+                    const updateDropDown = new DropDownList(DropdownListDetails)
+                    let insertMaster = await updateDropDown.updateOne({
+                        $set: {
+                            courseType: DropdownListDetails.courseType,
+                            popularCategories: DropdownListDetails.popularCategories,
+                            country: DropdownListDetails.country,
+                            offerTAT: DropdownListDetails.offerTAT,
+                            institutionType: DropdownListDetails.institutionType,
+                            paymentMethod: DropdownListDetails.paymentMethod,
+                            tax: DropdownListDetails.tax,
+                            commissionPaidOn: DropdownListDetails.commissionPaidOn,
+                            typeOfClient: DropdownListDetails.typeOfClient,
+
+                            modifiedOn: DropdownListDetails.modifiedOn,
+                            modifiedBy: DropdownListDetails.modifiedBy,
+                    }})
+                    response(req, res, activity, 'Level-2', 'Update-Master', true, 200, insertMaster, clientError.success.updateSuccess)
+                }
+                else {
+                    response(req, res, activity, 'Level-3', 'Update-Master', true, 422, {}, 'Email already registered');
+                }
+            } catch (err: any) {
+                response(req, res, activity, 'Level-3', 'Update-Master', false, 500, {}, errorMessage.internalServer, err.message)
+            }
+        } else {
+            response(req, res, activity, 'Level-3', 'Update-Master', false, 422, {}, errorMessage.fieldValidation, JSON.stringify(errors.mapped()));
+        }
+    };
+
+    export let deleteDropDownList = async (req, res, next) => {
+
         try {
-            const DropdownListDetails: DropDownListDocument = req.body;
-            const dropdown = await DropDownList.findOne({ $and: [{ _id: { $ne: DropdownListDetails._id }}] });
-           
-            if (!dropdown ) {
-                const updateDropDown = new DropDownList(DropdownListDetails)
-                let insertMaster = await updateDropDown.updateOne({
-                    $set: {
-                        courseType: DropdownListDetails.courseType,
-                        popularCategories: DropdownListDetails.popularCategories,
-                        country: DropdownListDetails.country,
-                        offerTAT: DropdownListDetails.offerTAT,
-                        institutionType: DropdownListDetails.institutionType,
-                        paymentMethod: DropdownListDetails.paymentMethod,
-                        tax: DropdownListDetails.tax,
-                        commissionPaidOn: DropdownListDetails.commissionPaidOn,
-                        typeOfClient: DropdownListDetails.typeOfClient,
-    
-                        modifiedOn: DropdownListDetails.modifiedOn,
-                        modifiedBy: DropdownListDetails.modifiedBy,
-                }})
-                response(req, res, activity, 'Level-2', 'Update-Master', true, 200, insertMaster, clientError.success.updateSuccess)
+            let id = req.query._id;
+            const country = await DropDownList.findByIdAndDelete({ _id: id })
+            response(req, res, activity, 'Level-2', 'Deleted the DropdownList', true, 200, country, 'Successfully Remove this Field');
+        }
+        catch (err: any) {
+            response(req, res, activity, 'Level-3', 'Deleted the DropdownList', false, 500, {}, errorMessage.internalServer, err.message);
+        }
+    };
+
+
+
+    export let getFilteredDropDown = async (req, res, next) => {
+        try {
+            var findQuery;
+            var andList: any = []
+            var limit = req.body.limit ? req.body.limit : 0;
+            var page = req.body.page ? req.body.page : 0;
+            andList.push({ isDeleted: false })
+            andList.push({ status: 1 })
+            if (req.body.courseType) {
+                andList.push({ courseType: req.body.courseType })
             }
-            else {
-                response(req, res, activity, 'Level-3', 'Update-Master', true, 422, {}, 'Email already registered');
+            if (req.body.popularCategories) {
+                andList.push({ popularCategories: req.body.popularCategories })
             }
+            if (req.body.offerTAT) {
+                andList.push({ offerTAT: req.body.offerTAT })
+            }
+            if (req.body.country) {
+                andList.push({ country: req.body.country })
+            }
+            if (req.body.institutionType) {
+                andList.push({ institutionType: req.body.institutionType })
+            }
+            if (req.body.paymentMethod) {
+                andList.push({ paymentMethod: req.body.paymentMethod })
+            }
+            if (req.body.tax) {
+                andList.push({ tax: req.body.tax })
+            }
+            if (req.body.commissionPaidOn) {
+                andList.push({ commissionPaidOn: req.body.commissionPaidOn })
+            }
+            if (req.body.typeOfClient) {
+                andList.push({ tax: req.body.typeOfClient })
+            }
+            findQuery = (andList.length > 0) ? { $and: andList } : {}
+
+            const dropDownList = await DropDownList.find(findQuery).sort({ createdAt: -1 }).limit(limit).skip(page)
+
+            const dropDownCount = await DropDownList.find(findQuery).count()
+            response(req, res, activity, 'Level-1', 'Get-Filter DropDown List', true, 200, { dropDownList, dropDownCount }, clientError.success.fetchedSuccessfully);
         } catch (err: any) {
-            response(req, res, activity, 'Level-3', 'Update-Master', false, 500, {}, errorMessage.internalServer, err.message)
+            response(req, res, activity, 'Level-3', 'Get-Filter DropDown List', false, 500, {}, errorMessage.internalServer, err.message);
         }
-    } else {
-        response(req, res, activity, 'Level-3', 'Update-Master', false, 422, {}, errorMessage.fieldValidation, JSON.stringify(errors.mapped()));
-    }
-};
-
-export let deleteDropDownList = async (req, res, next) => {
-
-    try {
-        let id = req.query._id;
-        const country = await DropDownList.findByIdAndDelete({ _id: id })
-        response(req, res, activity, 'Level-2', 'Deleted the DropdownList', true, 200, country, 'Successfully Remove this Field');
-    }
-    catch (err: any) {
-        response(req, res, activity, 'Level-3', 'Deleted the DropdownList', false, 500, {}, errorMessage.internalServer, err.message);
-    }
-};
-
-
-
-export let getFilteredDropDown = async (req, res, next) => {
-    try {
-        var findQuery;
-        var andList: any = []
-        var limit = req.body.limit ? req.body.limit : 0;
-        var page = req.body.page ? req.body.page : 0;
-        andList.push({ isDeleted: false })
-        andList.push({ status: 1 })
-        if (req.body.courseType) {
-            andList.push({ courseType: req.body.courseType })
-        }
-        if (req.body.popularCategories) {
-            andList.push({ popularCategories: req.body.popularCategories })
-        }
-        if (req.body.offerTAT) {
-            andList.push({ offerTAT: req.body.offerTAT })
-        }
-        if (req.body.country) {
-            andList.push({ country: req.body.country })
-        }
-        if (req.body.institutionType) {
-            andList.push({ institutionType: req.body.institutionType })
-        }
-        if (req.body.paymentMethod) {
-            andList.push({ paymentMethod: req.body.paymentMethod })
-        }
-        if (req.body.tax) {
-            andList.push({ tax: req.body.tax })
-        }
-        if (req.body.commissionPaidOn) {
-            andList.push({ commissionPaidOn: req.body.commissionPaidOn })
-        }
-        if (req.body.typeOfClient) {
-            andList.push({ tax: req.body.typeOfClient })
-        }
-        findQuery = (andList.length > 0) ? { $and: andList } : {}
-
-        const dropDownList = await DropDownList.find(findQuery).sort({ createdAt: -1 }).limit(limit).skip(page)
-
-        const dropDownCount = await DropDownList.find(findQuery).count()
-        response(req, res, activity, 'Level-1', 'Get-Filter DropDown List', true, 200, { dropDownList, dropDownCount }, clientError.success.fetchedSuccessfully);
-    } catch (err: any) {
-        response(req, res, activity, 'Level-3', 'Get-Filter DropDown List', false, 500, {}, errorMessage.internalServer, err.message);
-    }
-};
+    };
