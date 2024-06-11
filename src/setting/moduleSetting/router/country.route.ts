b0d2d3dd83bce946f6b641700734e1df015e1614
yuvarajud <119421277+yuvarajud@users.noreply.github.com>
2024-06-11 10:05:19 +0530

    in

--- src/setting/moduleSetting/router/country.route.ts
+++ src/setting/moduleSetting/router/country.route.ts
@@ -3,42 +3,32 @@ import { getAllCountry , getSingleCountry , createCountry , updateCountry , dele
 import { checkQuery, checkRequestBodyParams } from '../../../middleware/Validators';
 import { basicAuthUser } from '../../../middleware/checkAuth';
 
-
 const router: Router = Router();
 
 router.get('/',
     basicAuthUser,
     getAllCountry 
 );
-
 router.get('/getSingleCountry',
     basicAuthUser,
     checkQuery('_id'),
     getSingleCountry ,
 );
-
-
 router.post('/',
     basicAuthUser,
     createCountry 
 );
-
-
 router.put('/',
     basicAuthUser,
     checkQuery('_id'),
     updateCountry 
 );
-
-
 router.delete('/',
     basicAuthUser,
     checkQuery('_id'),
     deleteCountry 
 );
-
-
-router.put('/getFilterCountry ',
+router.put('/getFilterCountry',
     basicAuthUser,
     getFilteredCountry ,
 );
