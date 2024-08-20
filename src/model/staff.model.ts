a9934ad0f9f4ada4a32525e319c4bb06ffd7d095
Yuvaraj R <yuvarajr998@gmail.com>
2024-08-20 13:01:25 -0700

    inite

--- src/model/staff.model.ts
+++ src/model/staff.model.ts
@@ -7,7 +7,7 @@ export interface StaffDocument extends mongoose.Document {
     empName?: string;
     role?: string;
     dob?: String;
-    role?: string;
+   
     designation?: string;
     doj?: String;
     reportingManager?: string;
@@ -74,7 +74,7 @@ const staffSchema = new mongoose.Schema({
     photo: { type: String },
     role:{type:String},
     empName: { type: String },
-    role: {type: String},
+
     designation: { type: String },
     jobDescription: { type: String },
     reportingManager: { type: String },
