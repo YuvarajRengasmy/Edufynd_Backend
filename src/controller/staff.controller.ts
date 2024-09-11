19d6285fbabeda4d4ba940dd8df8d173fa9a6f93
Yuvaraj R <yuvarajr998@gmail.com>
2024-09-11 16:27:31 -0700

    inite

--- src/controller/staff.controller.ts
+++ src/controller/staff.controller.ts
@@ -367,7 +367,7 @@ export let getFilteredStaff = async (req, res, next) => {
         var limit = req.body.limit ? req.body.limit : 0;
         var page = req.body.page ? req.body.page : 0;
         andList.push({ isDeleted: false })
-        andList.push({ status: 1 })
+        // andList.push({ status: 1 })
         if (req.body.empName) {
             andList.push({ empName: req.body.empName })
         }
@@ -385,7 +385,7 @@ export let getFilteredStaff = async (req, res, next) => {
         }
         findQuery = (andList.length > 0) ? { $and: andList } : {}
 
-        const staffList = await Staff.find(findQuery).sort({employeeID: -1}).limit(limit).skip(page)
+        const staffList = await Staff.find(findQuery).sort({employeeID: -1}).limit(limit).skip(page).populate('adminId');
 
         const staffCount = await Staff.find(findQuery).count()
         response(req, res, activity, 'Level-1', 'Get-FilterStaff', true, 200, { staffList, staffCount }, clientError.success.fetchedSuccessfully);
