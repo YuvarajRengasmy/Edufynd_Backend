86110b46df93071bfa7798011520dd79a0853221
Yuvaraj R <yuvarajr998@gmail.com>
2024-10-03 16:40:21 -0700

    inite

--- src/controller/agent.controller.ts
+++ src/controller/agent.controller.ts
@@ -475,7 +475,6 @@ export let getFilteredStudentByAgent = async (req, res, next) => {
         var page = req.body.page ? req.body.page : 0;
         andList.push({ isDeleted: false })
         // andList.push({ status: 1 })
-
         if (req.body.studentId) {
             andList.push({ studentId: req.body.studentId })
         }
@@ -485,7 +484,7 @@ export let getFilteredStudentByAgent = async (req, res, next) => {
 
         findQuery = (andList.length > 0) ? { $and: andList } : {}
 
-        const agentList = await Agent.find(findQuery).sort({ agentCode: -1 }).limit(limit).skip(page).populate('studentId', { name: 1, email: 1, mobileNumber: 1 }).populate('adminId', { name: 1, shortName: 1 }).populate("agentId")
+        const agentList = await Agent.find(findQuery).sort({ agentCode: -1 }).limit(limit).skip(page).populate('studentId', { name: 1, email: 1, mobileNumber: 1 }).populate('adminId', { name: 1, shortName: 1 }).populate("agentName", { name: 1, shortName: 1 });
 
         const agentCount = await Agent.find(findQuery).count()
         response(req, res, activity, 'Level-1', 'Get-Filter', true, 200, { agentList, agentCount }, clientError.success.fetchedSuccessfully);
